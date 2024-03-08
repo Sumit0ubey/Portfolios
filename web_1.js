4272445ba59044191f92fd6726dc34e4ddcdcfbb
Sumit Dubey <149804568+Sumit0ubey@users.noreply.github.com>
2024-03-08 13:37:17 +0530

    Add files via upload

---
+++ web_1.js
@@ -0,0 +1,7 @@
+let typed = new Typed (".multuple-text", {
+    strings: ["Full-stack developer", "Student", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
